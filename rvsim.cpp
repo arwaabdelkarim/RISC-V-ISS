eb458c4602508821c499e026b076407998b74902
Yasmina-Mahdy <YasminaReda@aucegypt.edu>
2024-06-30 02:32:25 +0300

    fixed formatting and compilation errors

--- rvsim.cpp
+++ rvsim.cpp
@@ -21,13 +21,6 @@ unsigned int pc = 0;
 unsigned char memory[(64+64)*1024]; 
 
 
-registers reg[32] = { {"zero",0},{"ra",0},{"sp",4194304},{"gp",0},{"tp",0},
-					{"t0",0},{"t1",0},{"t2",0},{"s0",0},{"s1",0},
-					{"a0",0},{"a1",0},{"a2",0},{"a3",0},{"a4",0},{"a5",0},{"a6",0},{"a7",0},
-					{"s2",0},{"s3",0},{"s4",0},{"s5",0},{"s6",0},{"s7",0},{"s8",0},{"s9",0},
-					{"s10",0},{"s11",0},{"t3",0},{"t4",0},{"t5",0},{"t6",0} };
-
-
 
 void emitError(char *s)
 { 
@@ -59,7 +52,7 @@ int mem_to_reg(int ad, int i) {
 void instDecExec(unsigned int instWord)
 {
 	unsigned int rd, rs1, rs2, funct3, funct7, opcode;
-	unsigned int I_imm = 0x0, S_imm = 0x0, B_imm = 0x0, U_imm = 0x0, J_imm = 0x0, J_temp 0x0;
+	unsigned int I_imm = 0x0, S_imm = 0x0, B_imm = 0x0, U_imm = 0x0, J_imm = 0x0, J_temp = 0x0, temp;
 	unsigned int address;
 	unsigned int instPC = pc - 4;
 
@@ -75,32 +68,32 @@ void instDecExec(unsigned int instWord)
 	// Extraction of I immediate
 
 	I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
-	
+
 	//Extraction of B immediate 
-	B_imm = ((instWord >> 7) & 0x1); 
-    B_imm = (B_imm << 10);
-	B_imm = (B_imm | ((instWord >> 8) & 0x00F) | 
-		    ((instWord >> 21) & 0x3F0) |
-		    ((instWord >> 20) & 0x800));
+	B_imm = ((instWord >> 7) & 0x1);
+	B_imm = (B_imm << 10);
+	B_imm = (B_imm | ((instWord >> 8) & 0x00F) |
+		((instWord >> 21) & 0x3F0) |
+		((instWord >> 20) & 0x800));
 	B_imm = B_imm | ((instWord >> 31) ? 0xFFFFF800 : 0x0);
 	B_imm = B_imm << 1;
 
 	//Extraction of U immediate 
 	U_imm = ((instWord >> 12) & 0xFFFFF) | (((instWord >> 31) ? 0xFFF00000 : 0x0));
 
-  // - inst[31] - inst[30:25] - inst[11:7]
-	//temp = (((instWord >> 7) & 0x0000001F) | ((instWord >> 25 & 0x0000003F) << 5));
-	//S_imm = temp | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
+	// - inst[31] - inst[30:25] - inst[11:7]
+	  //temp = (((instWord >> 7) & 0x0000001F) | ((instWord >> 25 & 0x0000003F) << 5));
+	  //S_imm = temp | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
 
 	S_imm = (((instWord >> 7) & 0x0000001F) |
-			((instWord >> 20 & 0x00000FE0))) |
-			((instWord >> 31) ? 0xFFFFF800 : 0x0);
+		((instWord >> 20 & 0x00000FE0))) |
+		((instWord >> 31) ? 0xFFFFF800 : 0x0);
 
 	// extracts the immediate used for the jump and link instruction
-	J_temp = (instWord >> 20) & 0x000003FF; 
+	J_temp = (instWord >> 20) & 0x000003FF;
 	J_temp = J_temp << 1;
-	J_imm = J_imm | J_temp ;
-	J_temp = (instWord >> 19) & 0x00000001; 
+	J_imm = J_imm | J_temp;
+	J_temp = (instWord >> 19) & 0x00000001;
 	J_temp = J_temp << 11;
 	J_imm = J_imm | J_temp;
 	J_temp = (instWord >> 12) & 0x000000FF;
@@ -113,234 +106,306 @@ void instDecExec(unsigned int instWord)
 	J_imm |= (((instWord >> 31) ? 0xFFF00000 : 0x0));
 
 	// R Instructions
-	if(opcode == 0x33){	
-		funct7 = (instWord >> 25) & 0x0000007F;	
-		switch(funct3){
-			case 0: if(funct7 == 32)
-					{
-					   cout << "\tSUB\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-					   reg[rd].value = reg[rs1].value - reg[rs2].value;
-					}
-					else 
-					{
-						cout << "\tADD\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						reg[rd].value = reg[rs1].value + reg[rs2].value;
-					}
-					break;
+	if (opcode == 0x33) 
+	{
+		funct7 = (instWord >> 25) & 0x0000007F;
+		switch (funct3) 
+		{
+			case 0: 
+			{
+				if (funct7 == 32)
+				{
+					cout << "\tSUB\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+					reg[rd].value = reg[rs1].value - reg[rs2].value;
+				}
+				else
+				{
+					cout << "\tADD\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+					reg[rd].value = reg[rs1].value + reg[rs2].value;
+				}
+			}
+				break;
 
-			case 4: {
-						cout << "\tXOR\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						reg[rd].value = reg[rs1].value ^ reg[rs2].value;
-			        } 
-			         break;
-			case 6:  {
-						cout << "\tOR\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						reg[rd].value = reg[rs1].value | reg[rs2].value;
-			         }	
-			         break;
-			case 7:  {
-						cout << "\tAND\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						reg[rd].value = reg[rs1].value & reg[rs2].value;
-					 }
-			         break;
-			case 1: {
-						 cout << "\tSLL\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						 reg[rd].value = reg[rs1].value << reg[rs2].value;
-					}
-                     break;
-			case 5: if(funct7 == 32)
-					{
-						cout << "\tSRA\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						reg[rd].value = reg[rs1].value >> reg[rs2].value;
-					} 
-                    else
-					{
-						cout << "\tSRL\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						 reg[rd].value = (unsigned int)reg[rs1].value >> reg[rs2].value;
-					}
-					 break;
+			case 1: 
+			{
+				cout << "\tSLL\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+				reg[rd].value = reg[rs1].value << reg[rs2].value;
+			}
+				break;
 
-			case 2: {
-						 cout << "\tSLT\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						 reg[rd].value = (reg[rs1].value < reg[rs2].value) ? 1 : 0;
-					}	 
-				     break;
-			case 3:  {
-						 cout << "\tSLTU\t" << reg[rd].name <<","<<reg[rs1].name <<","<< reg[rs2].name << "\n";
-						 reg[rd].value = ((unsigned int)reg[rs1].value < (unsigned int)reg[rs2].value) ? 1 : 0;	 
-					 }
-			         break;
-			       				
+			case 2: 
+			{
+				cout << "\tSLT\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+				reg[rd].value = (reg[rs1].value < reg[rs2].value) ? 1 : 0;
+			}
+				  break;
+			case 3: 
+			{
+				cout << "\tSLTU\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+				reg[rd].value = ((unsigned int)reg[rs1].value < (unsigned int)reg[rs2].value) ? 1 : 0;
+			}
+				  break;
+
+			case 4: 
+			{
+				cout << "\tXOR\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+				reg[rd].value = reg[rs1].value ^ reg[rs2].value;
+			}
+				  break;
+
+			case 5: 
+			{
+				if (funct7 == 32)
+				{
+					cout << "\tSRA\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+					reg[rd].value = reg[rs1].value >> reg[rs2].value;
+				}
+				else
+				{
+					cout << "\tSRL\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+					reg[rd].value = (unsigned int)reg[rs1].value >> reg[rs2].value;
+				}
+			}
+				break;
+			case 6: 
+			{
+				cout << "\tOR\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+				reg[rd].value = reg[rs1].value | reg[rs2].value;
+			}
+				  break;
+			case 7: 
+			{
+				cout << "\tAND\t" << reg[rd].name << "," << reg[rs1].name << "," << reg[rs2].name << "\n";
+				reg[rd].value = reg[rs1].value & reg[rs2].value;
+			}
+				  break;
+		
 			default: cout << "\tUnkown R Instruction \n";
 		}
-	} 
+	}
 
-  else if(opcode == 0x13)  // I instructions
-	{	
-		switch(funct3)
+	// I instructions
+	else if (opcode == 0x13)  
+	{
+		switch (funct3)
 		{
-			case 0:	
+			case 0:
 			{
 				cout << "\tADDI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				reg[rd].value = reg[rs1].value + I_imm;
-				break;
 			}
+				break;
 			case 1:
 			{
 				cout << "\tSLLI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				I_imm = I_imm & 0x1F;
-				reg[rd].value = reg[rs1].value << I_imm;
-				break;
+				reg[rd].value = reg[rs1].value << (unsigned int)I_imm;
 			}
+				break;
 			case 2:
-			{ 
+			{
 				// not tested yet
 				cout << "\tSLTI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				reg[rd].value = (reg[rs1].value < I_imm) ? 1 : 0;
-				break;
 			}
+				break;
 			case 3:
 			{
 				// not tested yet
 				cout << "\tSLTIU\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				reg[rd].value = ((unsigned int)reg[rs1].value < I_imm) ? 1 : 0;
-                break;
 			}
+				break;
 			case 4:
-			{	
+			{
 				cout << "\tXORI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				reg[rd].value = reg[rs1].value ^ I_imm;
-				break;
 			}
+				break;
 			case 5:
 			{
 				if (funct7 == 0x20)
 				{
 					I_imm = I_imm & 0x1F;
 					cout << "\tSRAI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
-					reg[rd].value = reg[rs1].value >> I_imm;
+					reg[rd].value = reg[rs1].value >> (unsigned int)I_imm;
 					// cout << dec << reg[rd] << "\n"; for debugging
-				} 
+				}
 				else
 				{
 					I_imm = I_imm & 0x1F;
 					cout << "\tSRLI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
-					reg[rd].value = (unsigned int)reg[rs1].value >> I_imm;
-				} 
-				break;
+					reg[rd].value = (unsigned int)reg[rs1].value >> (unsigned int)I_imm;
+				}
 			}
+				break;
 			case 6:
 			{
 				cout << "\tORI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				reg[rd].value = reg[rs1].value | I_imm;
-				break;
 			}
+				break;
 			case 7:
 			{
 				cout << "\tANDI\t" << reg[rd].name << ", " << reg[rs1].name << ", " << dec << (int)I_imm << "\n";
 				reg[rd].value = reg[rs1].value & I_imm;
-				break;
 			}
+				break;
 			default:
 				cout << "\tUnkown I Instruction \n";
 		}
-  }  
-  
-	else if (opcode == 0x03) { // I - Load Instructions
-		int ad = reg[rs1].value + (int)I_imm; //stores the address of the LS byte we need
-		switch (funct3) {
-		case 0: cout << "\tLB\t" <<  reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
-			temp = memory[ad];
-			reg[rd].value = memory[ad] | ((temp >> 7) ? 0xFFFFFF00 : 0x0); // sign extending
-			break;
-		case 1:	cout << "\tLH\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
-			temp = mem_to_reg(ad, 1);
-			reg[rd].value = temp | ((temp >> 15)? (0xFFFF0000): 0x0); // sign extending
-			break;
-		case 2:	cout << "\tLW\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
-			reg[rd].value = mem_to_reg(ad, 3);
-			break;
-		case 4:	cout << "\tLBU\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
-			reg[rd].value = memory[ad]; // automatically zero extended because memory is unsigned char
-			break;
-		case 5:	cout << "\tLHU\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
-			reg[rd].value = mem_to_reg(ad, 1);
-			break;
-		default: 
-			cout<< "\tUnkown I-Load Instruction \n";
+	}
+
+	// I - Load Instructions
+	else if (opcode == 0x03) 
+	{ 
+		//stores the address of the LS byte we need
+		int ad = reg[rs1].value + (int)I_imm; 
+		switch (funct3) 
+		{
+			case 0: 
+			{
+				cout << "\tLB\t" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
+				temp = memory[ad];
+				// sign extending
+				reg[rd].value = memory[ad] | ((temp >> 7) ? 0xFFFFFF00 : 0x0); 
+			}
+				break;
+
+			case 1:	
+			{
+				cout << "\tLH\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
+				temp = mem_to_reg(ad, 1);
+				// sign extending
+				reg[rd].value = temp | ((temp >> 15) ? (0xFFFF0000) : 0x0); 
+			}
+				break;
+
+			case 2:	
+			{
+				cout << "\tLW\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
+				reg[rd].value = mem_to_reg(ad, 3);
+			}
+				break;
+
+			case 4:	
+			{
+				cout << "\tLBU\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
+				// automatically zero extended because memory is unsigned char
+				reg[rd].value = memory[ad];
+			}
+				break;
+
+			case 5:	
+			{
+				cout << "\tLHU\tx" << reg[rd].name << ", " << dec << (int)I_imm << "(" << reg[rs1].name << ")\n";
+				reg[rd].value = mem_to_reg(ad, 1);
+			}
+				break;
+
+			default:
+				cout << "\tUnkown I-Load Instruction \n";
 		}
 	}
-	else if (opcode == 0x23) {	// S Instructions
-		int ad = reg[rs1].value + (int)S_imm; //stores the address of the LS byte we need
+	// S Instructions
+	else if (opcode == 0x23) 	
+	{	
+		//stores the address of the LS byte we need
+		int ad = reg[rs1].value + (int)S_imm; 
 		switch (funct3) {
-		case 0:	cout << "\tSB\tx" << reg[rs2].name << ", " << dec << (int)S_imm << "(" << reg[rs1].name << ")\n";
-			memory[ad] = reg[rs2].value; // the memory address gets the LS byte in reg[rs2]
-			break;
-		case 1:	cout << "\tSH\tx" << reg[rs2].name << ", " << dec << (int)S_imm << "(" << reg[rs1].name << ")\n";
-			memory[ad] = reg[rs2].value;
-			memory[ad + 1] = reg[rs2].value >> 8;
-			break;
-		case 2: cout << "\tSW\tx" << reg[rs2].name << ", " << dec << (int)S_imm << "(" << reg[rs1].name << ")\n";
-			memory[ad] = reg[rs2].value;
-			memory[ad + 1] = reg[rs2].value >> 8;
-			memory[ad + 2] = reg[rs2].value >> 16;
-			memory[ad + 3] = reg[rs2].value >> 24;
-			break;
-		default:
-			cout << "\tUnkown S Instruction \n";
+			case 0:	
+			{
+				cout << "\tSB\tx" << reg[rs2].name << ", " << dec << (int)S_imm << "(" << reg[rs1].name << ")\n";
+				// the memory address gets the LS byte in reg[rs2]
+				memory[ad] = reg[rs2].value; 
+			}
+				break;
+
+			case 1:	
+			{
+				cout << "\tSH\tx" << reg[rs2].name << ", " << dec << (int)S_imm << "(" << reg[rs1].name << ")\n";
+				memory[ad] = reg[rs2].value;
+				memory[ad + 1] = reg[rs2].value >> 8;
+			}
+				break;
+
+			case 2: 
+			{
+				cout << "\tSW\tx" << reg[rs2].name << ", " << dec << (int)S_imm << "(" << reg[rs1].name << ")\n";
+				memory[ad] = reg[rs2].value;
+				memory[ad + 1] = reg[rs2].value >> 8;
+				memory[ad + 2] = reg[rs2].value >> 16;
+				memory[ad + 3] = reg[rs2].value >> 24;
+			}
+				break;
+
+			default:
+				cout << "\tUnkown S Instruction \n";
 		}
 
-	} 
-	else if (opcode == 0x63) // B instructions
+	}
+
+	// B instructions
+	else if (opcode == 0x63) 
 	{
-		switch(funct3){
-			case 0: 
-         {
-						cout << "\tBEQ\t" <<reg[rs1].name <<","<< reg[rs2].name << ","<< hex << "0x" << instPC+(int)B_imm << "\n";
-						pc = (reg[rs1].value == reg[rs2].value) ? instPC+(int)B_imm : pc; 
-				 }
-					break;
-			case 1: 
-        {
-						cout << "\tBNE\t" <<reg[rs1].name <<","<< reg[rs2].name <<","<< hex << "0x" << instPC+(int)B_imm <<"\n";
-						pc = (reg[rs1].value != reg[rs2].value) ? instPC+(int)B_imm : pc; 
-				}
-					break;
-        case 4:
-			{ 
+		switch (funct3) {
+			case 0:
+			{
+				cout << "\tBEQ\t" << reg[rs1].name << "," << reg[rs2].name << "," << hex << "0x" << instPC + (int)B_imm << "\n";
+				pc = (reg[rs1].value == reg[rs2].value) ? instPC + (int)B_imm : pc;
+			}
+			break;
+
+			case 1:
+			{
+				cout << "\tBNE\t" << reg[rs1].name << "," << reg[rs2].name << "," << hex << "0x" << instPC + (int)B_imm << "\n";
+				pc = (reg[rs1].value != reg[rs2].value) ? instPC + (int)B_imm : pc;
+			}
+			break;
+
+			case 4:
+			{
 				cout << "\tBLT\t" << reg[rs1].name << ", " << reg[rs2].name << ", " << hex << "0x" << instPC + (int)B_imm << "\n";
 				if (reg[rs1].value < reg[rs2].value)
-					pc = instPC + (int)B_imm;
-				break;
+					pc = instPC + (int)B_imm;	
 			}
+			break;
+
 			case 5:
 			{
-				cout << "\tBGE\t" << reg[rs1].name << ", " << reg[rs2].name << ", " <<  hex << "0x" << instPC + (int)B_imm << "\n";
-                if (reg[rs1].value >= reg[rs2].value)
+				cout << "\tBGE\t" << reg[rs1].name << ", " << reg[rs2].name << ", " << hex << "0x" << instPC + (int)B_imm << "\n";
+				if (reg[rs1].value >= reg[rs2].value)
 					pc = instPC + (int)B_imm;
-				break;
 			}
-       case 6: 
-        {
-          cout << "\tBLTU\tx" << reg[rs1].name << ", " << reg[rs2].name <<", 0x" << hex << (instPC + B_imm)<< "\n";
-          if ((unsigned int)reg[rs1].value < (unsigned int)reg[rs2].value) pc = instPC + (int)B_imm;
-        }
 			break;
-		  case 7: 
-        {
-          cout << "\tBGEU\tx" << reg[rs1].name << ", " << reg[rs2].name <<", 0x" << hex << (instPC + B_imm) << "\n";
-			    if ((unsigned int)reg[rs1].value >= (unsigned int)reg[rs2].value) pc = instPC + (int)B_imm;
-        }
+
+			case 6:
+			{
+				cout << "\tBLTU\tx" << reg[rs1].name << ", " << reg[rs2].name << ", 0x" << hex << (instPC + B_imm) << "\n";
+				if ((unsigned int)reg[rs1].value < (unsigned int)reg[rs2].value) pc = instPC + (int)B_imm;
+			}
 			break;
+
+			case 7:
+			{
+				cout << "\tBGEU\tx" << reg[rs1].name << ", " << reg[rs2].name << ", 0x" << hex << (instPC + B_imm) << "\n";
+				if ((unsigned int)reg[rs1].value >= (unsigned int)reg[rs2].value) pc = instPC + (int)B_imm;
+			}
+			break;
+
 			default:
-						cout << "\tUnkown B Instruction \n";	
+				cout << "\tUnkown B Instruction \n";
 		}
-    else if (opcode == 0x37) //not done yet
+	}
+
+	// U instructions 
+	else if (opcode == 0x37) //not done yet
 	{
 		cout << "\tLUI\t" << reg[rd].name << ", " << dec << ((int)U_imm << 12) << "\n"; //shifting 12 bits to the right to load to upper 20 bits in rd
 		reg[rd].value = (int)U_imm;
 		cout << reg[rd].value << endl;
 	}
+
 	else if (opcode == 0x17) //not done yet 
 	{
 		cout << "\tAUIPC\t" << reg[rd].name << ", " << dec << ((int)U_imm << 12) << "\n";
@@ -348,47 +413,54 @@ void instDecExec(unsigned int instWord)
 		cout << reg[rd].value << endl;
 	}
 
-	else if(opcode == 0x6F)
+	// J instructions
+	else if (opcode == 0x6F)
 	{
 
-		cout << "\tJAL\t" <<reg[rd].name <<","<< hex << "0x" << instPC+(int)J_imm <<"\n";
+		cout << "\tJAL\t" << reg[rd].name << "," << hex << "0x" << instPC + (int)J_imm << "\n";
 		reg[rd].value = pc;
 		pc = instPC + J_imm;
 	}
-	else if(opcode == 0x67)
+
+	else if (opcode == 0x67)
 	{
-		cout << "\tJALR\t" <<reg[rd].name <<","<< reg[rs1].name <<","<< hex << "0x" << reg[rs1].value + (int)I_imm <<"\n";
+		cout << "\tJALR\t" << reg[rd].name << "," << reg[rs1].name << "," << hex << "0x" << reg[rs1].value + (int)I_imm << "\n";
 		reg[rd].value = pc;
 		pc = reg[rs1].value + (int)I_imm;
 	}
-	else if (opcode == 73) 
-  {
+
+	// Ecall instructions
+	else if (opcode == 73)
+	{
 		if (reg[17].value == 1)  // a7 = 1
-			 cout << reg[10].value;	
+			cout << reg[10].value;
 		else if (reg[17].value == 4)
-    {
+		{
 			int ad = reg[10].value;
 			char c = memory[ad]; // c = char at the address stored in a0
 			string output = "";
 
-			while (c != 0) 
-      { // as long as we havent reached a null
+			while (c != 0)
+			{ // as long as we havent reached a null
 				output += c;
 				ad++;
 				c = memory[ad];
-		  }
+			}
 			cout << output;
 		}
-		else 
-    {
+		else
+		{
 			cout << "\tUnsupported Ecall Functionality\n";
 		}
 	}
-	else 
-  {
-      cout << "\tUnkown Instruction \n";
-	} 
+
+	else
+	{
+		cout << "\tUnkown Instruction \n";
+	}
 }
+
+
 
 int main(int argc, char *argv[]){
 
