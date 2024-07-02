a38ca080e94f1a5a0560d54ee259df4fbc499d5e
Rana Taher <124688927+rana5679@users.noreply.github.com>
2024-07-02 21:57:23 +0300

    Update rvsim.cpp

--- rvsim.cpp
+++ rvsim.cpp
@@ -498,12 +498,12 @@ void instDecExec(unsigned int instWord)
 	}
 }
 
-void Decompress(unsigned int insthalfWord)
+void Decompress(unsigned int insthalf)
 {
 
 // after decompressing the instruction halfword we will call the other function
 
-	instDecExec(insthalfWord);
+	instDecExec(insthalf);
 }
 
 
