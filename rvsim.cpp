1d702295cec9f3448d20306e513b060534784dca
rana5679 <124688927+rana5679@users.noreply.github.com>
2024-07-05 14:02:36 +0300

    updating compressed instructions

--- rvsim.cpp
+++ rvsim.cpp
@@ -13,9 +13,9 @@ struct registers
 };
 
 
-registers reg[32]={{"zero",0},{"ra",0},{"sp",4194304},{"gp",0},{"tp",0},{"t0",0},{"t1",0},{"t2",0},{"s0",0},{"s1",0},{"a0",0},{"a1",0},
-				   {"a2",0},{"a3",0},{"a4",0},{"a5",0},{"a6",0},{"a7",0},{"s2",0},{"s3",0},{"s4",0},{"s5",0},{"s6",0},{"s7",0},{"s8",0},
-				   {"s9",0},{"s10",0},{"s11",0},{"t3",0},{"t4",0},{"t5",0},{"t6",0}};
+registers reg[32]={{"zero",0},{"ra"},{"sp"},{"gp"},{"tp"},{"t0"},{"t1"},{"t2"},{"s0"},{"s1"},{"a0"},{"a1"},
+				   {"a2"},{"a3"},{"a4"},{"a5"},{"a6"},{"a7"},{"s2"},{"s3"},{"s4"},{"s5"},{"s6"},{"s7"},{"s8"},
+				   {"s9"},{"s10"},{"s11"},{"t3"},{"t4"},{"t5"},{"t6"}};
 
 unsigned int pc = 0;
 unsigned char memory[(64+64)*1024]; 
@@ -49,52 +49,15 @@ int mem_to_reg(int ad, int i) {
 	return x;
 }
 
-unsigned int Decompress(unsigned int instHalf)
-{
-	unsigned int opcode, rs2, rs1, rd, func4, rd_dash, rs1_dash, rs2_dash, offset, func3;
-	unsigned int CJ_imm = 0x0, CI_imm = 0x0, CSS_imm = 0x0, CIW_imm = 0x0, CL_imm = 0x0, CS_imm = 0x0, CB_imm = 0x0;
-	
-	opcode = instHalf & 0x00000003;
-	rs2 = (instHalf >> 2) & 0x0000001F;
-	rd = (instHalf>> 7) & 0x0000001F;
-	rs1 = (instHalf>> 7) & 0x0000001F;
-	func4 = (instHalf>> 12) & 0x0000000F;
-	func3 = (instHalf>> 13) & 0x00000007;
-	rd_dash = (instHalf>> 2) & 0x00000007;
-	rs2_dash = (instHalf>> 2) & 0x00000007;
-	rs1_dash = (instHalf>> 7) & 0x00000007;
 
-	// Extraction of CI immediate
-	instHalf= 0b0000000001010100;
-	CI_imm = (instHalf>> 2) & 0x001F;
-	CI_imm |= ((instHalf>> 7) & 0x0020);
-	CI_imm = CI_imm | ((instHalf>> 12) ? 0xFFFFFFC0 : 0x00000000);
 
-	// CJ offset
-	CJ_imm =((instHalf>>= 2) & 0x1);
-	CJ_imm <<= 4;
-	CJ_imm = (CJ_imm | ((instHalf>> 3) & 0x00F)|
-					   ((instHalf>> 2) & 0x3F0));
-	CJ_imm = CJ_imm | ((instHalf>> 12) ? 0xFFFFF800 : 0X0);
-	CJ_imm <<= 1;
-	
-
-	if (opcode == 0x1)
-	{
-	
-		
-	}
-
-	// after decompressing the instruction halfword we will call the other function to execute the instruction
-	instDecExec(instHalf);
-}
-
-void instDecExec(unsigned int instWord)
+void instDecExec(unsigned int instWord, bool compressed)
 {
 	unsigned int rd, rs1, rs2, funct3, funct7, opcode;
 	unsigned int I_imm = 0x0, S_imm = 0x0, B_imm = 0x0, U_imm = 0x0, J_imm = 0x0, J_temp = 0x0, temp;
 	unsigned int address;
 	unsigned int instPC = pc - 4;
+	unsigned int instPC_C = pc - 2;
 
 	printPrefix(instPC, instWord);
 
@@ -130,18 +93,24 @@ void instDecExec(unsigned int instWord)
 		((instWord >> 31) ? 0xFFFFF800 : 0x0);
 
 	//Extraction of J immediate 
-	J_temp = (instWord >> 20) & 0x000003FF;
-	J_temp = J_temp << 1;
-	J_imm = J_imm | J_temp;
-	J_temp = (instWord >> 19) & 0x00000001;
-	J_temp = J_temp << 11;
-	J_imm = J_imm | J_temp;
-	J_temp = (instWord >> 12) & 0x000000FF;
-	J_temp = J_temp << 12;
-	J_imm = J_imm | J_temp;
-	J_temp = (instWord >> 31) & 0x00000001;
-	J_temp = J_temp << 20;
-	J_imm = J_imm | J_temp;
+	// J_temp = (instWord >> 20) & 0x000003FF;
+	// J_temp = J_temp << 1;
+	// J_imm = J_imm | J_temp;
+	// J_temp = (instWord >> 19) & 0x00000001;
+	// J_temp = J_temp << 11;
+	// J_imm = J_imm | J_temp;
+	// J_temp = (instWord >> 12) & 0x000000FF;
+	// J_temp = J_temp << 12;
+	// J_imm = J_imm | J_temp;
+	// J_temp = (instWord >> 31) & 0x00000001;
+	// J_temp = J_temp << 20;
+	// J_imm = J_imm | J_temp;
+
+	J_imm = ((instWord >> 31 & 0x1 ) << 20) |
+			((instWord >> 21 & 0x3FF) << 1) |
+			((instWord >> 20 & 0x1) << 11) |
+			((instWord >> 12 & 0xFF) << 12);
+	J_imm |= (((instWord >> 31) ? 0xFFF00000 : 0x0));
 
 	J_imm |= (((instWord >> 31) ? 0xFFF00000 : 0x0));
 
@@ -488,16 +457,14 @@ void instDecExec(unsigned int instWord)
 	else if (opcode == 0x6F)
 	{
 		cout << "\tJAL\t" << reg[rd].name << "," << hex << "0x" << instPC + (int)J_imm << "\n";
-		pc = instPC + J_imm;
-		if (rd == 0) return; // to keep reg zero unchanged
-		reg[rd].value = pc;
+		if (rd != 0) reg[rd].value = pc; // to keep reg zero unchanged
+		pc = (compressed)? instPC_C+J_imm : instPC + J_imm;
 	}
 	else if (opcode == 0x67)
 	{
 		cout << "\tJALR\t" << reg[rd].name << "," << reg[rs1].name << "," << hex << "0x" << reg[rs1].value + (int)I_imm << "\n";
-		pc = reg[rs1].value + (int)I_imm;
-		if (rd == 0) return; // to keep reg zero unchanged
-		reg[rd].value = pc;
+		if (rd != 0) reg[rd].value = pc; // to keep reg zero unchanged
+		pc =reg[rs1].value + (int)I_imm;
 	}
   
 	// Ecall instruction
@@ -536,6 +503,300 @@ void instDecExec(unsigned int instWord)
 	}
 }
 
+void Decompress(unsigned int instHalf)
+{
+	unsigned int opcode, rs2, rs1, rd, func4, rd_dash, rs1_dash, rs2_dash, offset, func3, func, func6, instWord = 0x0;
+	unsigned int CJ_imm = 0x0, CI_imm = 0x0, CSS_imm = 0x0, CIW_imm = 0x0, CL_imm = 0x0, CS_imm = 0x0, CB_imm = 0x0;
+	
+	opcode = instHalf & 0x00000003;
+	rs2 = (instHalf >> 2) & 0x0000001F;
+	rd = (instHalf>> 7) & 0x0000001F;
+	rs1 = (instHalf>> 7) & 0x0000001F;
+	func4 = (instHalf>> 12) & 0x0000000F;
+	func3 = (instHalf>> 13) & 0x00000007;
+	rd_dash = (instHalf>> 2) & 0x00000007;
+	rs2_dash = (instHalf>> 2) & 0x00000007;
+	rs1_dash = (instHalf>> 7) & 0x00000007;
+	func = (instHalf>> 5) & 0x00000003;
+	func6 = (instHalf>> 10) & 0x003F;
+	rd_dash = rd_dash | 0x08;
+	rs2_dash = rs2_dash | 0x08;
+	rs1_dash = rs1_dash | 0x08;
+
+	unsigned int instPC = pc - 2;
+
+	printPrefix(instPC, instHalf);
+
+	// CSS immediate
+	CSS_imm = (instHalf >> 9) & 0x00F;
+	CSS_imm = CSS_imm | (((instHalf >> 7) & 0x0003) << 4);
+	CSS_imm = CSS_imm << 2;
+	CSS_imm = CSS_imm | (0x0 << 8);
+
+	// CL immediate
+	CL_imm = (instHalf >> 6) & 0x0001;
+	CL_imm = CL_imm | (((instHalf >> 10) & 0x0007) << 1);
+	CL_imm = CL_imm | (((instHalf >> 5) & 0x0001) << 4);
+	CL_imm = CL_imm << 2;
+	CL_imm = CL_imm | (0x0000 << 7);
+
+	// CS immediate 
+	CS_imm = (instHalf >> 6) & 0x0001;
+	CS_imm = CS_imm | (((instHalf >> 10) & 0x0007) << 1);
+	CS_imm = CS_imm | (((instHalf >> 5) & 0x0001) << 4);
+	CS_imm = CS_imm << 2;
+	CS_imm = CS_imm | (0x0000 << 7);
+
+
+
+	if (opcode == 0x1)
+	{
+		switch (func)
+		{
+			case 0:
+			{
+				// c.sub
+				if(func6 == 0x23)
+				{
+					// opcode
+					instWord = 0x33;
+					// rd
+					instWord = instWord | (rs1_dash << 7);
+					// fucntion 3
+					instWord = instWord | (0x0 << 12);
+					// rs1
+					instWord = instWord | (rs1_dash << 15);
+					// rs2
+					instWord = instWord | (rs2_dash << 20);
+					// function 7
+					instWord = instWord | (0x20 << 25);
+
+					cout<<"\tC.SUB\t"<<reg[rs1_dash].name<<","<< reg[rs2_dash].name << endl;
+				}
+
+					
+			}
+				break;	
+
+			case 1:
+			{
+				// c.xor
+				if(func6 == 0x23)
+				{
+					// opcode
+					instWord = 0x33;
+					// rd
+					instWord = instWord | (rs1_dash << 7);
+					// function 3
+					instWord = instWord | (0x4 << 12);
+					// rs1
+					instWord = instWord | (rs1_dash << 15);
+					// rs2
+					instWord = instWord | (rs2_dash << 20);
+					// function 7
+					instWord = instWord | (0x00 << 25);
+
+					cout<<"\tC.XOR\t"<<reg[rs1_dash].name<<","<< reg[rs2_dash].name << endl;
+				}
+			}
+				break;
+
+			case 2:
+			{
+				// c.or
+				if(func6 == 0x23)
+				{
+					// opcode
+					instWord = 0x33;
+					// rd
+					instWord = instWord | (rs1_dash << 7);
+					// function 3
+					instWord = instWord | (0x6 << 12);
+					// rs1
+					instWord = instWord | (rs1_dash << 15);
+					// rs2
+					instWord = instWord | (rs2_dash << 20);
+					// function 7
+					instWord = instWord | (0x00 << 25);
+
+					cout<<"\tC.OR\t"<<reg[rs1_dash].name<<","<< reg[rs2_dash].name << endl;
+				}
+			}
+				break;
+
+			case 3:
+			{
+				// c.and
+				if(func6 == 0x23)
+				{
+					// opcode
+					instWord = 0x33;
+					// rd
+					instWord = instWord | (rs1_dash << 7);
+					// function 3
+					instWord = instWord | (0x7 << 12);
+					// rs1
+					instWord = instWord | (rs1_dash << 15);
+					// rs2
+					instWord = instWord | (rs2_dash << 20);
+					// function 7
+					instWord = instWord | (0x00 << 25);
+
+					cout<<"\tC.AND\t"<<reg[rs1_dash].name<<","<< reg[rs2_dash].name << endl;
+				}
+			}	
+				break;	
+
+			default:
+			        cout<<"\tunknown compressed instruction\t"<<endl;	
+
+		}
+
+		switch (func3)
+		{
+			case 3:
+			{
+				 //addisp16
+				if (rd == 2)
+				{
+					// adding the base (opcode -- rd -- funct3 -- rs1 )
+					instWord = 0x00010113;
+					// extracting the immediate
+
+					// adding the immediate
+					instWord |= ((CI_imm << 20) & 0xFFF00000);
+
+				}
+
+				// c.lui
+				else if(rd != 0)
+				{
+					// CI immediate
+					CI_imm = (instHalf >> 2) & 0x001F;
+					CI_imm = CI_imm |(((instHalf >> 12) & 0x0001) << 5);
+					CI_imm = CI_imm << 12;
+					CI_imm |= (CI_imm & 0x020000) ? 0xFFC0000 : 0x0;
+
+					// opcode
+					instWord = 0x37;
+					// rd
+					instWord = instWord | (rd << 7);
+					// imm[31:12]
+					instWord = instWord | (CI_imm << 12);
+
+					cout<<"\tC.LUI\t"<<reg[rs1].name<<","<< CI_imm << endl;
+				}
+			}
+				break;
+
+			case 0:
+			{
+				if(rs1 == 0) // c.nop
+				{}
+				// c.addi
+				else
+				{
+					CI_imm = (instHalf >> 2) & 0x001F;
+					CI_imm = CI_imm |(((instHalf >> 12) & 0x0001) << 5);
+					CI_imm |= (CI_imm & 0x0020) ? 0xFFC0 : 0x0;
+					cout<<"\tC.ADDI\t"<<reg[rs1].name<<","<< CI_imm << endl;
+
+					CI_imm = CI_imm << 20;
+
+					instWord = 0x13;
+					instWord = instWord | (rs1 << 7);
+					instWord = instWord | (0x0 <<12);
+					instWord = instWord | (rs1 <<15);
+					instWord = instWord | CI_imm;
+
+					
+				}
+			}
+
+			default: cout<<"\tunknown compressed instruction\t"<<endl;	
+		}	
+	}
+
+	else if (opcode == 0x2)
+	{
+		switch (func3)
+		{
+			// c.swsp
+			case 6:
+			{
+				// opcode
+				instWord = 0x23;
+				// imm [4:0]
+				instWord = instWord | ((CSS_imm & 0x01F) << 7);
+				// function 3
+				instWord = instWord | (0x3 << 12);
+				// stack pointer
+				instWord = instWord | (0x02 << 15);
+				// rs2
+				instWord = instWord | (rs2 << 20);
+				// imm[11:5]
+				instWord = instWord | ((CSS_imm >> 5) & 0x07F);
+
+				cout<<"\tC.SWSP\t"<<reg[rs2].name<<","<< CSS_imm << endl;
+			}
+				break;
+
+
+			default: cout<<"\tunknown compressed instruction\t"<<endl;	
+
+		}
+	}
+
+	else if (opcode == 0x0)
+	{
+		switch (func3)
+		{
+			// c.lw
+			case 2:
+			{
+				// opcode
+				instWord = 0x02;
+				// rd
+				instWord = instWord | (rd_dash << 7);
+				// function 3
+				instWord = instWord | (func3 << 12);
+				// rs1
+				instWord = instWord | (rs1_dash << 15);
+				// imm[11:0]
+				instWord = instWord | (CL_imm << 20);
+
+				cout<<"\tC.LW\t"<<reg[rd_dash].name<<","<< CL_imm <<"("<<reg[rs1_dash].name<<")"<<endl;
+			}
+				break;
+
+			// c.sw
+			case 6:
+			{
+				// opcode
+				instWord = 0x23;
+				// imm[4:0]
+				instWord = instWord | ((CS_imm & 0x001F) << 7);
+				// function 3
+				instWord = instWord | (0x2 << 12);
+				// rs1
+				instWord = instWord | (rs1_dash << 15);
+				// rs2
+				instWord = instWord | (rs2_dash << 20);
+				// imm[11:5]
+				instWord = instWord | (((CS_imm >> 5) & 0x007F) << 25);
+
+				cout<<"\tC.SW\t"<<reg[rs2_dash].name<<","<< CS_imm <<"("<<reg[rs1_dash].name<<")"<<endl;
+			}
+				break;
+
+			default: cout<<"\tunknown compressed instruction\t"<<endl;	
+		}
+	}
+
+	// after decompressing the instruction halfword we will call the other function to execute the instruction
+	instDecExec(instWord,1);
+}
+
 int main(int argc, char *argv[])
 {
 	unsigned int instWord = 0;
@@ -567,18 +828,6 @@ int main(int argc, char *argv[])
 			emitError("Cannot read from data file\n");
 	}
 
-	//cout << a << endl; for debugging purposes
-	/*instWord = 0000000110000000;
-	unsigned int CJ_imm =((instWord >>= 2) & 0x1);
-	CJ_imm <<= 4;
-	CJ_imm = (CJ_imm | ((instWord >> 3) & 0x00F)|
-					   ((instWord >> 2) & 0x7E0));
-	CJ_imm = CJ_imm | ((instWord >> 12) ? 0xFFFFF800 : 0X0);
-	CJ_imm <<= 1;
-	bitset<12> x;
-	x = CJ_imm; 
-	cout << x << endl;
-	cout << dec << CJ_imm << endl;*/ 
 
 	if(inFile.is_open())
 	{
@@ -601,7 +850,7 @@ int main(int argc, char *argv[])
 				instWord = instWord | (((unsigned char)memory[pc])<<16) |
 			               (((unsigned char)memory[pc+1])<<24);
 				pc += 2;
-				instDecExec(instWord);
+				instDecExec(instWord,0);
 			}	
 			else
 			    Decompress(instWord);
